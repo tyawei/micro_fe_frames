7a558821953f8f26ae5810c8e243e1da049cbfd9
唐亚维 <tangyawei@jinzhenzongdeMacBook-Air.local>
2021-07-25 10:41:25 +0800

    feat:整理服务端

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var ejs = require('ejs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,14 +11,18 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+
+// view engine setup
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html'); 
+app.set('views', path.join(__dirname, '../public'));
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
